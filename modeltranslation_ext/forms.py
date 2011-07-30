ec0ea887744943306dd7b5c1fccd3d77b2316e84
ivan <none@none>
2011-07-30 19:27:31 +0300

    pep8 + refactory

--- modeltranslation_ext/forms.py
+++ modeltranslation_ext/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from utils import formfield_exclude_translations
 
+
 class TranslationModelForm(forms.ModelForm):
-    
+    """Shows localized form"""
     def formfield_callback(self, **kwargs):
+        """formfield_exclude_translations implementation"""
         return formfield_exclude_translations(self, **kwargs)
-    
--- modeltranslation_ext/utils.py
+++ modeltranslation_ext/utils.py
@@ -1,24 +1,36 @@
-from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, string_concat
 from modeltranslation.translator import translator
 from modeltranslation.utils import get_language, build_localized_fieldname
 
 
 def formfield_exclude_translations(db_field, **kwargs):
+    """Filter form and keep only non-localized fields"""
     if hasattr(db_field, 'translated_field'):
         return None
-    
-    field = db_field.formfield(**kwargs)
+
+    if 'field' in kwargs:
+        field = kwargs['field']
+    else:
+        field = db_field.formfield(**kwargs)
+
     if not field:
         return field
+
     trans_opts = translator.get_options_for_model(db_field.model)
     if db_field.name in trans_opts.fields:
-        field.widget.attrs['class'] = getattr(field.widget.attrs, 'class', '') + ' language-depended'
-        field.help_text = u'{0} {1}'.format(field.help_text, ugettext('This field dependent on current language.'))
-    
+        field.widget.attrs['class'] = u'{0} {1}'.format(
+            getattr(field.widget.attrs, 'class', ''),
+            'language-depended'
+        )
+        field.help_text = string_concat(
+            field.help_text,
+            _('This field dependent on current language.')
+        )
     return field
 
+
 def localize_fieldname(field_name, lang=None):
+    """Localize fieldname"""
     if lang is None:
         lang = get_language()
     return build_localized_fieldname(field_name, lang)
-
