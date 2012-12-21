76863c676aa42b49378361f50472ee2f6af050b9
ivan <none@none>
2012-12-22 01:07:00 +0200

    Added new variants of form representation

--- modeltranslation_ext/forms.py
+++ modeltranslation_ext/forms.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from django import forms
-from .utils import formfield_exclude_translations
+from .utils import (formfield_exclude_translations,
+    formfield_exclude_original, formfield_exclude_irrelevant)
 
 
 class TranslationModelForm(forms.ModelForm):
@@ -8,3 +9,17 @@ class TranslationModelForm(forms.ModelForm):
     def formfield_callback(self, **kwargs):
         """formfield_exclude_translations implementation"""
         return formfield_exclude_translations(self, **kwargs)
+
+
+class TranslationBulkModelForm(forms.ModelForm):
+    """Shows localized form"""
+    def formfield_callback(self, **kwargs):
+        """formfield_exclude_translations implementation"""
+        return formfield_exclude_original(self, **kwargs)
+
+
+class TranslationActualModelForm(forms.ModelForm):
+    """Shows localized form"""
+    def formfield_callback(self, **kwargs):
+        """formfield_exclude_translations implementation"""
+        return formfield_exclude_irrelevant(self, **kwargs)
--- modeltranslation_ext/utils.py
+++ modeltranslation_ext/utils.py
@@ -31,6 +31,55 @@ def formfield_exclude_translations(db_field, **kwargs):
     return field
 
 
+def formfield_exclude_original(db_field, **kwargs):
+    """Filter form and keep only localized fields"""
+    trans_opts = translator.get_options_for_model(db_field.model)
+    if db_field.name in trans_opts.fields:
+        return None
+
+    if 'field' in kwargs:
+        field = kwargs['field']
+    else:
+        field = db_field.formfield(**kwargs)
+
+    if hasattr(db_field, 'translated_field'):
+        if db_field.name.endswith('_{0}'.format(get_language())):
+            field.required = True
+        else:
+            field.required = False
+
+    return field
+
+
+def formfield_exclude_irrelevant(db_field, **kwargs):
+    """Filter form and keep only localized fields"""
+    trans_opts = translator.get_options_for_model(db_field.model)
+    if db_field.name in trans_opts.fields:
+        return None
+
+    if 'field' in kwargs:
+        field = kwargs['field']
+    else:
+        field = db_field.formfield(**kwargs)
+
+    if hasattr(db_field, 'translated_field'):
+        if db_field.name.endswith('_{0}'.format(get_language())):
+            field.required = True
+            field.widget.attrs['class'] = '{0} {1}'.format(
+                getattr(field.widget.attrs, 'class', ''),
+                'language-depended'
+            )
+            field.help_text = string_concat(
+                field.help_text,
+                _(' '),
+                _('This field dependent on current language.')
+            )
+        else:
+            return None
+
+    return field
+
+
 def localize_fieldname(field_name, lang=None):
     """Localize fieldname"""
     if lang is None:
